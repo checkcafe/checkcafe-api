1e052c905923cec22159b505944bb0b6468faa65
Zacky Achmad <zckyachmd@gmail.com>
2024-10-20 16:37:50 +0700

    refactor: cafe to place on schema prisma

--- prisma/migrations/20241020094507_create_cafe_schema/migration.sql
+++ prisma/migrations/20241020094507_create_cafe_schema/migration.sql
@@ -1,6 +1,3 @@
--- CreateEnum
-CREATE TYPE "Day" AS ENUM ('MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY');
-
 -- CreateTable
 CREATE TABLE "countries" (
     "id" TEXT NOT NULL,
@@ -27,9 +24,9 @@ CREATE TABLE "states" (
 CREATE TABLE "cities" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "stateId" TEXT NOT NULL,
     "latitude" DOUBLE PRECISION,
     "longitude" DOUBLE PRECISION,
+    "stateId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -37,41 +34,28 @@ CREATE TABLE "cities" (
 );
 
 -- CreateTable
-CREATE TABLE "cafes" (
+CREATE TABLE "places" (
     "id" TEXT NOT NULL,
-    "cityId" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT,
     "latitude" DOUBLE PRECISION,
     "longitude" DOUBLE PRECISION,
     "streetAddress" VARCHAR(255) NOT NULL,
+    "cityId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "cafes_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "cafe_photos" (
-    "id" TEXT NOT NULL,
-    "cafeId" TEXT NOT NULL,
-    "cafeFeatureId" TEXT,
-    "url" TEXT NOT NULL,
-    "order" INTEGER NOT NULL DEFAULT 0,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "cafe_photos_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "places_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "operating_hours" (
     "id" TEXT NOT NULL,
-    "cafeId" TEXT NOT NULL,
-    "day" "Day" NOT NULL,
-    "startTime" TEXT NOT NULL,
-    "endTime" TEXT NOT NULL,
+    "day" TEXT NOT NULL,
+    "startDateTime" TIMESTAMP(3) NOT NULL,
+    "endDateTime" TIMESTAMP(3) NOT NULL,
+    "placeId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -92,9 +76,9 @@ CREATE TABLE "feature_categories" (
 -- CreateTable
 CREATE TABLE "features" (
     "id" TEXT NOT NULL,
-    "featureCategoryId" TEXT,
     "name" TEXT NOT NULL,
     "description" TEXT,
+    "featureCategoryId" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -102,28 +86,52 @@ CREATE TABLE "features" (
 );
 
 -- CreateTable
-CREATE TABLE "cafe_features" (
+CREATE TABLE "place_features" (
     "id" TEXT NOT NULL,
-    "featureId" TEXT NOT NULL,
-    "cafeId" TEXT NOT NULL,
     "description" TEXT,
+    "featureId" TEXT NOT NULL,
+    "placeId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "cafe_features_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "place_features_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "cafe_reviews" (
+CREATE TABLE "place_photos" (
     "id" TEXT NOT NULL,
-    "cafeId" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "rating" INTEGER NOT NULL,
-    "comment" TEXT,
+    "url" TEXT NOT NULL,
+    "order" INTEGER NOT NULL DEFAULT 0,
+    "placeId" TEXT NOT NULL,
+    "placeFeatureId" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "cafe_reviews_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "place_photos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "place_favorites" (
+    "id" TEXT NOT NULL,
+    "placeId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "place_favorites_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "place_reviews" (
+    "id" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "comment" TEXT,
+    "placeId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "place_reviews_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -139,13 +147,13 @@ CREATE UNIQUE INDEX "states_name_key" ON "states"("name");
 CREATE UNIQUE INDEX "cities_name_key" ON "cities"("name");
 
 -- CreateIndex
-CREATE INDEX "cafe_photos_cafeId_order_idx" ON "cafe_photos"("cafeId", "order");
-
--- CreateIndex
 CREATE UNIQUE INDEX "feature_categories_name_key" ON "feature_categories"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "features_name_key" ON "features"("name");
+
+-- CreateIndex
+CREATE INDEX "place_photos_placeId_order_idx" ON "place_photos"("placeId", "order");
 
 -- AddForeignKey
 ALTER TABLE "states" ADD CONSTRAINT "states_countryId_fkey" FOREIGN KEY ("countryId") REFERENCES "countries"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -154,31 +162,37 @@ ALTER TABLE "states" ADD CONSTRAINT "states_countryId_fkey" FOREIGN KEY ("countr
 ALTER TABLE "cities" ADD CONSTRAINT "cities_stateId_fkey" FOREIGN KEY ("stateId") REFERENCES "states"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "cafes" ADD CONSTRAINT "cafes_cityId_fkey" FOREIGN KEY ("cityId") REFERENCES "cities"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "places" ADD CONSTRAINT "places_cityId_fkey" FOREIGN KEY ("cityId") REFERENCES "cities"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "cafes" ADD CONSTRAINT "cafes_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "places" ADD CONSTRAINT "places_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "cafe_photos" ADD CONSTRAINT "cafe_photos_cafeId_fkey" FOREIGN KEY ("cafeId") REFERENCES "cafes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "cafe_photos" ADD CONSTRAINT "cafe_photos_cafeFeatureId_fkey" FOREIGN KEY ("cafeFeatureId") REFERENCES "cafe_features"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "operating_hours" ADD CONSTRAINT "operating_hours_cafeId_fkey" FOREIGN KEY ("cafeId") REFERENCES "cafes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "operating_hours" ADD CONSTRAINT "operating_hours_placeId_fkey" FOREIGN KEY ("placeId") REFERENCES "places"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "features" ADD CONSTRAINT "features_featureCategoryId_fkey" FOREIGN KEY ("featureCategoryId") REFERENCES "feature_categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "cafe_features" ADD CONSTRAINT "cafe_features_featureId_fkey" FOREIGN KEY ("featureId") REFERENCES "features"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "place_features" ADD CONSTRAINT "place_features_featureId_fkey" FOREIGN KEY ("featureId") REFERENCES "features"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "cafe_features" ADD CONSTRAINT "cafe_features_cafeId_fkey" FOREIGN KEY ("cafeId") REFERENCES "cafes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "place_features" ADD CONSTRAINT "place_features_placeId_fkey" FOREIGN KEY ("placeId") REFERENCES "places"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "cafe_reviews" ADD CONSTRAINT "cafe_reviews_cafeId_fkey" FOREIGN KEY ("cafeId") REFERENCES "cafes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "place_photos" ADD CONSTRAINT "place_photos_placeId_fkey" FOREIGN KEY ("placeId") REFERENCES "places"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "cafe_reviews" ADD CONSTRAINT "cafe_reviews_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "place_photos" ADD CONSTRAINT "place_photos_placeFeatureId_fkey" FOREIGN KEY ("placeFeatureId") REFERENCES "place_features"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "place_favorites" ADD CONSTRAINT "place_favorites_placeId_fkey" FOREIGN KEY ("placeId") REFERENCES "places"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "place_favorites" ADD CONSTRAINT "place_favorites_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "place_reviews" ADD CONSTRAINT "place_reviews_placeId_fkey" FOREIGN KEY ("placeId") REFERENCES "places"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "place_reviews" ADD CONSTRAINT "place_reviews_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
